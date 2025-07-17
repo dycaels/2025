1ace96acd2071bf96e2d781fdce5299574592c82
dycaels <dycaels.github@gmail.com>
2025-07-17 14:02:30 -0300

    teste

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export', // importante!
   basePath: '/2026', // igual ao nome do repositório no GitHub
-  assetPrefix: '/2026/',
   images: {
     unoptimized: true // necessário se estiver usando next/image
   },
